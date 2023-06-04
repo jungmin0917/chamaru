3e59be1391bc2df68cdd4e9c9f43534e36430b2b
jungmin0917 <cloonds@gmail.com>
2023-06-04 23:04:44 +0900

    차마루 - 전체적인 맥락 잡고 회의 중 역할 분담 완료. 현수님 - 회원가입, 로그인 / 성혁님 - 차량 등록 / 현우님 - 아직

---
+++ src/main/java/com/chamaru/service/CarService.java
@@ -0,0 +1,12 @@
+package com.chamaru.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CarService {
+
+}
