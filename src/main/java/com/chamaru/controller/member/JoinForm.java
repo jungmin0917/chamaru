7680f8e2a83e10297d3e02b7cc3c79e312f9f92f
khs0808sky <khs0808sky@naver.com>
2023-06-09 16:52:00 +0900

    ※작업한 내용※
     1. resources - templates - outlines(header, footer), layouts(main)추가
    
     2. entity - member부분 수정(컬럼)
    	고유번호 추가
    	테이블 바꾸기 -> spring.jpa.hibernate.ddl-auto=create로 바꾸고 다시 update로 바꿈
    
     3. resources - templates - member(join.html)추가
    
     4. controller - member - JoinForm.java 추가
    
     5. resources - messages - commons.properties, validations.properties 추가
    
     6. config - MvcConfig 추가
    
     7. controller - member - MemberController 추가
    
     8. pom.xml에 타임리프 레이아웃 의존성주입
    
     9. ChamaruApplication의 @EnableJpaAuditing을 제거해서 MvcConfig에 추가

---
+++ src/main/java/com/chamaru/controller/member/JoinForm.java
@@ -0,0 +1,29 @@
+package com.chamaru.controller.member;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class JoinForm {
+
+    @NotBlank
+    @Size(min = 8)
+    private String userId;
+
+    @NotBlank
+    @Size(min = 8)
+    private String userPw;
+
+    @NotBlank
+    private String userPwRe;
+
+    @NotBlank
+    private String userNm;
+
+    @Email
+    private String userEmail;
+
+    private String userPhone;
+}
